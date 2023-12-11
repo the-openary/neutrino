59040f87a10f1c731c9319f0029ad09c2659120c
dari-studios <dari@daristudios.ca>
2023-12-10 19:43:14 -0500

    added license

--- firmware/esp8266/firmware.ino
+++ firmware/esp8266/firmware.ino
@@ -1,19 +1,5 @@
 /*
-  ESP8266 firmware for the Neutrio 3D Printer
-    Copyright (C) 2023 Alexander Ren
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  ESP8266 firmware for the Neutrino 3D Printer.
 */
 
 #include <ESP8266WiFi.h>
